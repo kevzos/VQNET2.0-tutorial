6d45b47bcda811fb1b865f33cb7bfb3520668cb9
王汉超 王汉超 <whc@originqc.com>
2025-02-20 16:07:44 +0800

    [feat] first commit, delete some files, v0.1

--- source/conf.py
+++ source/conf.py
@@ -18,7 +18,7 @@ copyright = '2022, Original Quantum'
 author = 'Original Quantum'
 
 # The full version, including alpha/beta/rc tags
-release = 'v2.15.0'
+release = 'v0.1'
 
 
 # -- General configuration ---------------------------------------------------
